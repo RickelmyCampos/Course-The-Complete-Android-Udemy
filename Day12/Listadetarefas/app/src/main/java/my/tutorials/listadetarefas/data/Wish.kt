c2b9e3165e94f0cb769c26e49fba0a6e1ac7c8d4
Gilberson Campos <gra.campos@discente.ufma.br>
2024-02-07 09:22:09 -0300

    add config database table

--- Day12/Listadetarefas/app/src/main/java/my/tutorials/listadetarefas/data/Wish.kt
+++ Day12/Listadetarefas/app/src/main/java/my/tutorials/listadetarefas/data/Wish.kt
@@ -1,7 +1,20 @@
 package my.tutorials.listadetarefas.data
 
-data class Wish(val id: Long = 0L, val title: String = "", val description: String = "")
-object DummyWish{
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "wish-table")
+data class Wish(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,
+    @ColumnInfo(name = "wish-title")
+    val title: String = "",
+    @ColumnInfo(name = "wish-desc")
+    val description: String = ""
+)
+
+object DummyWish {
     val wishList: List<Wish> = listOf(
         Wish(title = "Google", description = "hello Google"),
         Wish(title = "Google", description = "hello Google"),
